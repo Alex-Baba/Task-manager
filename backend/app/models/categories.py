a2fac952c474e861787fe2cb94db1d2c8068d6f1
Alex-Baba <alexbaba18@gmail.com.com>
2026-04-17 15:01:10 +0300

    add categories model

--- backend/app/models/categories.py
+++ backend/app/models/categories.py
@@ -1,4 +1,9 @@
+import uuid
 from enum import Enum
+from sqlalchemy import Column, String, Enum as SAEnum, String
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+from .base import Base, BaseModel
 
 
 class Category(str,Enum):
@@ -12,3 +17,12 @@ class Category(str,Enum):
     OTHER = 'OTHER'
 
 
+class Categories(Base, BaseModel):
+    __tablename__ = 'categories'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(SAEnum(Category), unique=True, nullable=False)
+    description = Column(String(255), nullable=True)
+
+    # tasks relationship
+    tasks = relationship('Task', back_populates='category', lazy='selectin')
